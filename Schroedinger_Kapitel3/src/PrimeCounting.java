d09fd37ac55f3a0b7a36781f1e7bc9c8cebb6ce6
Hannes Jacob <hjac101@web.de>
2022-03-27 13:01:36 +0200

    Commit VM

---
+++ Schroedinger_Kapitel3/src/PrimeCounting.java
@@ -0,0 +1,17 @@
+public class PrimeCounting {
+    public static void main(String[] args) {
+
+        for(int i=1; i<100;i++){
+            boolean istPrime = true;
+            for(int j=2; j<i;j++){
+                if(i%j==0){
+                    istPrime = false;
+                    break;
+                }
+            }
+            if(istPrime){
+                System.out.println(i);
+            }
+        }
+    }
+}
